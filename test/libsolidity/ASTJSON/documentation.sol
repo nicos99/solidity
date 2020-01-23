446a0b3953a5f4f84591ec7ab139fcb070547390
Erik Kundt <erik@ethereum.org>
2020-01-23 12:22:11 +0100

    Adjusts JSON import and export to new Natspec AST node.

--- test/libsolidity/ASTJSON/documentation.sol
+++ test/libsolidity/ASTJSON/documentation.sol
@@ -1,19 +1,23 @@
 // ---- SOURCE: a
 
-/**This contract is empty*/ contract C {}
+/**This contract is empty*/
+contract A {}
 
 // ---- SOURCE: b
 
 /**This contract is empty
  and has a line-breaking comment.*/
-contract C {}
+contract B {}
 
 // ---- SOURCE: c
 
 contract C {
-  /** Some comment on Evt.*/ event Evt();
-  /** Some comment on mod.*/ modifier mod() { _; }
-  /** Some comment on fn.*/ function fn() public {}
+  /** Some comment on Evt.*/
+  event Evt();
+  /** Some comment on mod.*/
+  modifier mod() { _; }
+  /** Some comment on fn.*/
+  function fn() public {}
 }
 
 // ----
